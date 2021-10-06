0aac8dc225a79be89a8ca8a9c84f26fae88bf67e
Jesus6Connex <jesus.velazquez@6connex.com>
2021-10-06 17:53:09 -0500

    Create TheAppLayout.vue

---
+++ frontend/src/components/TheAppLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container fluid class="mt-16">
+    <h2 class="text-h4 my-6">{{ title }}</h2>
+    <p class="text-subtitle-2 mb-6">{{ subtitle }}</p>
+    <slot> </slot>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "TheAppLayout",
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: () => {
+        "";
+      },
+    },
+    subtitle: {
+      type: String,
+      required: false,
+      default: () => {
+        "";
+      },
+    },
+  },
+};
+</script>
