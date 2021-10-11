cb6dbff701e2037a6ab297ed98907f7ff322c4dc
Jesus6Connex <jesus.velazquez@6connex.com>
2021-10-10 21:06:00 -0500

    connect contract on load

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <v-app>
     <the-nav-bar />
+    <v-alert type="error" class="mt-12 mb-4" v-if="state.error.message">
+      <h6 class="overline">{{ state.error.name }}</h6>
+      <p class="">{{ state.error.message }}</p>
+    </v-alert>
     <v-container>
       <router-view />
     </v-container>
@@ -10,10 +14,24 @@
 <script>
 import TheNavBar from "./components/TheNavBar.vue";
 
+import { mapGetters, mapActions } from "vuex";
+
 export default {
   name: "ConsultaPopular",
   components: {
     TheNavBar,
+  },
+  computed: {
+    ...mapGetters({ state: "getState" }),
+  },
+  methods: {
+    ...mapActions(["checkWeb3Availability", "connectContract"]),
+  },
+  mounted() {},
+  async created() {
+    document.title = "Consulta Popular";
+    this.checkWeb3Availability();
+    await this.connectContract();
   },
 };
 </script>
