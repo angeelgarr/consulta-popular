840c4aee1477c2867c2ceccc641532767ec98524
Jesus6Connex <jesus.velazquez@6connex.com>
2021-10-13 17:38:34 -0500

    add filter to check if wallet is connected

--- frontend/src/views/Resultados.vue
+++ frontend/src/views/Resultados.vue
@@ -1,32 +1,44 @@
 <template>
   <the-app-layout title="Resultados" :subtitle="`Actualizado: ${new Date()}`">
-    <v-data-table
-      :headers="headers"
-      :items="state.options"
-      class="elevation-1"
-      disable-pagination
-      disable-filtering
-      hide-default-footer
-    ></v-data-table>
-
-    <v-card class="pa-2 pa-sm-4 mt-6">
-      <h3 class="text-h6 my-4">
-        Total de votos registrados:
-        {{ totalVotes }}
-      </h3>
-      <div class="primary px-2 px-sm-4  py-6 py-sm-10 white--text rounded">
-        <v-sparkline
-          :value="chartData"
-          :labels="labels"
-          :gradient="['#DDC9A3', '#BC955C']"
-          type="bar"
-          line-width="85"
-          auto-draw
-          smooth="10"
+    <template v-if="!state.account">
+      <v-container fluid fill-height class="d-flex justify-center">
+        <v-btn
+          color="primary"
+          v-if="!state.account"
+          @click="connectWalletAndFetchResults"
         >
-        </v-sparkline>
-      </div>
-    </v-card>
+          Conecta tu monedero
+        </v-btn>
+      </v-container>
+    </template>
+    <template v-else>
+      <v-data-table
+        :headers="headers"
+        :items="state.options"
+        class="elevation-1"
+        disable-pagination
+        disable-filtering
+        hide-default-footer
+      ></v-data-table>
+      <v-card class="pa-2 pa-sm-4 mt-6">
+        <h3 class="text-h6 my-4">
+          Total de votos registrados:
+          {{ totalVotes }}
+        </h3>
+        <div class="primary px-2 px-sm-4  py-6 py-sm-10 white--text rounded">
+          <v-sparkline
+            :value="chartData"
+            :labels="labels"
+            :gradient="['#DDC9A3', '#BC955C']"
+            type="bar"
+            line-width="85"
+            auto-draw
+            smooth="10"
+          >
+          </v-sparkline>
+        </div>
+      </v-card>
+    </template>
   </the-app-layout>
 </template>
 
@@ -63,12 +75,16 @@ export default {
     },
   },
   methods: {
-    ...mapActions(["getVotes"]),
+    ...mapActions(["getVotes", "connectWallet"]),
     ...mapMutations(["clearError"]),
+    async connectWalletAndFetchResults() {
+      await this.connectWallet();
+      this.getVotes();
+    },
   },
   async mounted() {
     this.clearError();
-    await this.getVotes();
+    if (this.state.account) await this.getVotes();
   },
 };
 </script>
